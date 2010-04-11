6d9845065e4f1f3bf3f641d48c2c2244acb812a4
Joel Charlesworth <joelc@jrctech.com>
2010-04-11 13:09:27 -0600

    Everything up to Task T: Testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  
+  layout "store"
+    
   before_filter :authorize, :except => :login
+  before_filter :set_locale
   
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
@@ -11,9 +13,25 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
-  layout "store"
-  
 protected
+  
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale]
+    I18n.locale = session[:locale] || I18n.default_locale
+    
+    locale_path = "#{LOCALES_DIRECTORY}#{I18n.locale}.yml"
+    
+    unless I18n.load_path.include? locale_path
+      I18n.load_path << locale_path
+      I18n.backend.send(:init_translations)
+    end
+  rescue Exception => err
+    logger.error err
+    flash.now[:notice] = "#{I18n.locale} translation not available"
+    
+    I18n.load_path -= [locale_path]
+    I18n.locale = session[:locale] = I18n.default_locale
+  end
 
   def authorize
     unless User.find_by_id(session[:user_id])
