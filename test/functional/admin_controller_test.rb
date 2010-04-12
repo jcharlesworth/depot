d346340936d5fa1904294379f3e1bbc837eef477
Joel Charlesworth <joelc@jrctech.com>
2010-04-12 09:51:46 -0600

    working on testing

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :users
+
+  test "index" do
+    get :index
+    assert_redirected_to :action => "login"
+    assert_equal "Please log in", flash[:notice]
   end
+
+  test "index with user" do
+    get :index, {}, { :user_id => users(:dave).id }
+    assert_response :success
+    assert_tempate "index"
+  end
+  
 end
